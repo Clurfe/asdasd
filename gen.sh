826fd4b2bb337f5cb5fe9eae1c426fa771dcd471
ZyCromerZ <neetroid97@gmail.com>
2023-03-08 02:15:38 +0700

    update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- gen.sh
+++ gen.sh
@@ -1,6 +1,10 @@
 if [[ ! -z "${CONFL}" ]];then
     cd YouTube-Viewer
-    python3 prox_check2.py
+    pip install -U git+https://github.com/bluet/proxybroker2.git
+    proxybroker find --types HTTP HTTPS SOCKS4 SOCKS5 -l 40 -c ID -o get.txt
+    cat get.txt | awk -F "] " '{print $2}' | awk -F ">" '{print $1}' > GoodProxy.txt
+    proxybroker find --types HTTP HTTPS SOCKS4 SOCKS5 -l 700 -o get.txt
+    cat get.txt | awk -F "] " '{print $2}' | awk -F ">" '{print $1}' >> GoodProxy.txt
 else
     echo "config null?"
 fi
--- prep.sh
+++ prep.sh
@@ -1,16 +1,14 @@
 if [[ ! -z "${CONFL}" ]];then
-    sudo apt update && sudo apt install wget
+    sudo apt install wget
     git clone https://github.com/MShawon/YouTube-Viewer.git --depth 10
     cd YouTube-Viewer
-    python3 -m pip install --upgrade pip wheel
-    pip3 install "setuptools<59"
     pip3 install -r requirements.txt
-    for asu in config.json new.txt urls.txt search.txt
+    for asu in config.json urls.txt search.txt
     do
         rm -rf $asu
         wget ${CONFL}/$asu -O $asu
     done 
-    wget https://raw.githubusercontent.com/Clurfe/prox-gen/master/result.txt -O GoodProxy.txt
+    wget https://raw.githubusercontent.com/Clurfe/prox-gen/master/result-checked.txt -O GoodProxy.txt
 else
     echo "config null?"
 fi
